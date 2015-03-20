5a557e2c1ca9e8db23d597146d982a82164f603d
Mickael Hubert <m.hubert@iia-laval.fr>
2015-03-20 16:08:56 +0100

    Add tag version

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
-### application.bat - 2015 - Mickael Hubert ###
-echo "version 1.1"
-echo "Build : 10:00"
-echo "Copyright company - 2015"
+### application.bat - 2015 - votre nom ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
